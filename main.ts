31db549bf1f66f2eaa6a9918d19a76454c7f719d
marco-fossen <92363779+marco-fossen@users.noreply.github.com>
2021-12-22 17:28:20 +0100

    Aggiorna main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,5 +1,5 @@
 let portaAperta = 0
-pins.servoWritePin(AnalogPin.P0, 0)
+pins.servoWritePin(AnalogPin.P2, 0)
 pins.digitalWritePin(DigitalPin.P8, 1)
 pins.digitalWritePin(DigitalPin.P12, 1)
 pins.digitalWritePin(DigitalPin.P16, 1)
@@ -8,8 +8,16 @@ basic.forever(function () {
     if (pins.digitalReadPin(DigitalPin.P1) == 0) {
         basic.showIcon(IconNames.Square)
         if (portaAperta == 0) {
-            for (let indice = 0; indice <= 90; indice++) {
-                pins.servoWritePin(AnalogPin.P0, indice)
+            soundExpression.mysterious.play()
+            pins.digitalWritePin(DigitalPin.P12, 1)
+            basic.pause(1000)
+            pins.digitalWritePin(DigitalPin.P12, 0)
+            pins.digitalWritePin(DigitalPin.P8, 1)
+            basic.pause(1000)
+            pins.digitalWritePin(DigitalPin.P8, 0)
+            pins.digitalWritePin(DigitalPin.P12, 0)
+            for (let indice = 0; indice <= 180; indice++) {
+                pins.servoWritePin(AnalogPin.P2, indice)
                 pins.digitalWritePin(DigitalPin.P8, randint(0, 1))
                 pins.digitalWritePin(DigitalPin.P12, randint(0, 1))
                 pins.digitalWritePin(DigitalPin.P16, randint(0, 1))
@@ -19,12 +27,11 @@ basic.forever(function () {
             pins.digitalWritePin(DigitalPin.P8, 1)
             pins.digitalWritePin(DigitalPin.P12, 1)
             pins.digitalWritePin(DigitalPin.P16, 1)
-            pins.servoWritePin(AnalogPin.P0, 180)
         }
     } else {
         basic.showIcon(IconNames.No)
         portaAperta = 0
-        pins.servoWritePin(AnalogPin.P0, 0)
+        pins.servoWritePin(AnalogPin.P2, 0)
         pins.digitalWritePin(DigitalPin.P8, 0)
         pins.digitalWritePin(DigitalPin.P12, 0)
         pins.digitalWritePin(DigitalPin.P16, 0)
